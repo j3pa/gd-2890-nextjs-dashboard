d99a677d89f1e5594fc055896f6266bf2fa8fb12
j3pa <yeezepah@gmail.com>
2026-05-28 17:00:47 +0700

    benerin eror

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,7 +18,6 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  // Mengikat ID invoice ke server action
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
   return (
